4dc680519f4225daf91a802922fc084d3cfb34b8
DylanK00275517 <k00275517@student.lit.ie>
2024-02-22 11:42:20 +0000

    New version of react

---
+++ beOurGuest main/src/Pages/home.jsx
@@ -0,0 +1,17 @@
+import './home.css'
+
+export default function home(){
+   return (
+   <div className='home-pageDiv'>
+   
+   <header className='header-title'>
+   
+   <h1>Where Every Guest Becomes Part of the Family</h1> 
+   <h3>Get to Know, Get Excited, Get Ready for the Celebration!</h3>
+   </header>
+   <div className='button-div'>
+   <a href="/signUp"> <button >Get started </button></a> 
+   </div>
+   </div>
+   )
+}
