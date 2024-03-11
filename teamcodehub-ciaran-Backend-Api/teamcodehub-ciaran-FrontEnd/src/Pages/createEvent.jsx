4d38bd7f4fb8bf6fa0be3795c4aaa9f80130063c
DaraPhillips <114760819+DaraPhillips@users.noreply.github.com>
2024-03-11 10:08:01 +0000

    Scroll to top - Media queries
    Changes to App.jsx - scrollToTop
    
    Changes to Home.css - Media queries

--- teamcodehub-ciaran-Backend-Api/teamcodehub-ciaran-FrontEnd/src/Pages/createEvent.jsx
+++ teamcodehub-ciaran-Backend-Api/teamcodehub-ciaran-FrontEnd/src/Pages/createEvent.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Link, Navigate } from 'react-router-dom';
-import './createEventStyle.css';
+import { Navigate } from 'react-router-dom';
 import axios from 'axios';
 import SvgVenue from '../Icons/SvgVenue';
-import SvgCreate  from '../Icons/SvgCreate';
+import SvgCreate from '../Icons/SvgCreate';
 import SvgTime from '../Icons/SvgTime';
 import SvgBellIcon from '../Icons/SvgBellIcon';
 import SvgReset from '../Icons/SvgReset';
+import './createEventStyle.css';
 
 export default function CreateEvent() {
     const [eventData, setEventData] = useState({
@@ -15,7 +15,6 @@ export default function CreateEvent() {
         address1: '',
         address2: '',
         address3: '',
-
         zip: '',
         respondByDate: '',
         eventType: '2',
@@ -26,7 +25,6 @@ export default function CreateEvent() {
     const [countries, setCountries] = useState([]);
 
     useEffect(() => {
-        // Fetch countries from backend when component mounts
         axios.get('http://127.0.0.1:8000/countries/')
             .then(response => {
                 setCountries(response.data);
@@ -34,25 +32,20 @@ export default function CreateEvent() {
             .catch(error => {
                 console.error('Error fetching countries:', error);
             });
-    }, []); // Empty dependency array to run only once on mount
+    }, []);
 
-    // Event handler for form submission
     const handleSubmit = async (event) => {
         event.preventDefault();
 
         try {
-            // Send POST request to create event
             const response = await axios.post('http://127.0.0.1:8000/create_event/', { event: eventData });
             console.log('Event created successfully:', response.data);
-            // Optionally redirect to dashboard or show a success message
             setEventCreated(true);
         } catch (error) {
             console.error('Error creating event:', error);
-            // Optionally show an error message
         }
     };
 
-    // Event handler for input changes
     const handleChange = (event) => {
         const { name, value } = event.target;
         setEventData(prevData => ({
@@ -76,35 +69,41 @@ export default function CreateEvent() {
             <div className='event-col1'>
                 <div className='event-details-container'>
                     <form className='host-form' onSubmit={handleSubmit}>
-                        {/* VENUE DETAILS */}
                         <label className="Venue-deets" htmlFor="venue-details">Venue details</label>
+
                         <div className='details-group'>
-                            <select name="country" id="country-id" value={eventData.country} onChange={handleChange}>
-                                <option key="" value="">Select Country</option>
-                                {countries.map(country => (
-                                    <option key={country.countriesId} value={country.countriesId}>{country.countryName}</option>
-                                ))}
-                            </select>
+                            <div className='input-container'>
+                                <div className='icon'><SvgVenue /></div>
+                                <input type="text" name="venue" id="venue-id" placeholder=" Venue" onChange={handleChange} />
+                            </div>
                         </div>
 
                         <div className='details-group'>
-                            <input type="text" name="venue" id="venue-id" placeholder=" Venue" onChange={handleChange} />
+                            <div className='input-container'>
+                                <div className='icon'><SvgVenue /></div>
+                                <input type="text" name="address1" id="address1-id" placeholder=" Address line 1" onChange={handleChange} />
+                            </div>
                         </div>
 
                         <div className='details-group'>
-                            <input type="text" name="address1" id="address1-id" placeholder=" Address line 1" onChange={handleChange} />
+                            <div className='input-container'>
+                                <div className='icon'><SvgVenue /></div>
+                                <input type="text" name="address2" id="address2-id" placeholder=" Address line 2" onChange={handleChange} />
+                            </div>
                         </div>
 
                         <div className='details-group'>
-                            <input type="text" name="address2" id="address2-id" placeholder=" Address line 2" onChange={handleChange} />
+                            <div className='input-container'>
+                                <div className='icon'><SvgVenue /></div>
+                                <input type="text" name="address3" id="address3-id" placeholder=" Address line 3" onChange={handleChange} />
+                            </div>
                         </div>
 
                         <div className='details-group'>
-                            <input type="text" name="address3" id="address3-id" placeholder=" Address line 3" onChange={handleChange} />
-                        </div>
-
-                        <div className='details-group'>
-                            <input type="text" name="zip" id="Zip-id" placeholder=" Zip code" onChange={handleChange} />
+                            <div className='input-container'>
+                                <div className='icon'><SvgVenue /></div>
+                                <input type="text" name="zip" id="Zip-id" placeholder=" Zip code" onChange={handleChange} />
+                            </div>
                         </div>
 
                         <label htmlFor="username">Respond by date </label>
@@ -114,20 +113,26 @@ export default function CreateEvent() {
 
                         <label htmlFor="username">Enter wedding time</label>
                         <div className='details-group'>
-                            <input type="time" id="time" name="time" onChange={handleChange} required />
+                            <div className='input-container'>
+                                <div className='icon'><SvgTime /></div>
+                                <input type="time" id="time" name="time" onChange={handleChange} required />
+                            </div>
                         </div>
 
                         <label htmlFor="username">Enter wedding date  </label>
                         <div className='details-group'>
-                            <input type="date" id="date" name="date" onChange={handleChange} />
+                            <div className='input-container'>
+                                <div className='icon'></div>
+                                <input type="date" id="date" name="date" onChange={handleChange} />
+                            </div>
                         </div>
 
-                         <div className='reset-deets'>
+                        <div className='reset-deets'>
                             <button className='resetBtn' type="submit">Reset my information</button>
                         </div>
 
                         <div className='event-details-buttoncontainer'>
-                            <button className='createEvent-button' type="submit">Create event <SvgCreate /> </button>
+                            <button className='createEvent-button' type="submit">Create event <SvgCreate /></button>
                         </div>
                     </form>
                 </div>
