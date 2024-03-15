478366901cd5d10e1de540e50f7e7cc04458b1e5
cjoc40 <cjoc40@hotmail.com>
2024-03-15 12:53:19 +0000

    reorder

--- teamcodehub-ciaran-Backend-Api(DP)/teamcodehub-ciaran-FrontEnd/src/Pages/features.jsx
+++
@@ -1,9 +0,0 @@
-export default function features(){
-    return (
-
-        <div className='topyoke'>
-<h3 className='home-tab-top-thingy'>Home / </h3> <h3 className='other-tab-at-top'>Features</h3>
-</div>
-
-    )
- }
