6c60359e0c80094f7d579a820ea19ecdc8286aa7
StevenNolan1 <k00228896@student.lit.ie>
2024-04-11 09:50:35 +0100

    Update settings.py

--- be_our_guest_website/be_our_guest_website/settings.py
+++ be_our_guest_website/be_our_guest_website/settings.py
@@ -122,6 +122,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'beourguest48@gmail.com'
+EMAIL_HOST_PASSWORD = 'dphq ogej ifej rccb'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
